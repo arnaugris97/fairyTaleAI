d4e03962f573cb092f979b6b0b19d2022a973019
carlescamps <c.camps16@gmail.com>
2024-06-19 20:04:25 +0200

    FTAI:16 - more changes

--- create_embedding_DB.py
+++ create_embedding_DB.py
@@ -1,21 +1,20 @@
-# create_embedding_DB.py
-
 import pandas as pd
 import chromadb
 from inference_utils import load_model, preprocess_input, generate_embeddings
 from tokenizer.wordPieceTokenizer import WordPieceTokenizer
 
 class ChromaEmbeddingProcessor:
-    def __init__(self, model_path, csv_file):
+    def __init__(self, model_path, tokenizer_path, csv_file):
         self.model_path = model_path
+        self.tokenizer_path = tokenizer_path
         self.csv_file = csv_file
         self.chromadb_client = chromadb.Client()
-        self.model, _ = self.load_bert_model()  # Load BERT model
+        self.model, self.tokenizer = self.load_bert_model()  # Load BERT model and tokenizer
         self.model.eval()  # Set the model to evaluation mode
-        self.custom_tokenizer = WordPieceTokenizer()  # Initialize tokenizer
 
     def load_bert_model(self):
-        return load_model(self.model_path, None)
+        # Initialize model and tokenizer
+        return load_model(self.model_path, self.tokenizer_path)
 
     def load_sentences_from_csv(self):
         df = pd.read_csv(self.csv_file)
@@ -26,11 +25,20 @@ class ChromaEmbeddingProcessor:
     def process_and_store_embeddings(self):
         sentences, titles = self.load_sentences_from_csv()
         embeddings = []
-
-        for sentence, title in zip(sentences, titles):
-            inputs = preprocess_input(sentence, self.custom_tokenizer)
-            sentence_embeddings = generate_embeddings(inputs, self.model).numpy().tolist()
+        num_sentences = len(sentences)
+        
+        for i, (sentence, title) in enumerate(zip(sentences, titles)):
+            # Ensure sentence is a string
+            sentence = str(sentence)
+            inputs = preprocess_input(sentence, self.tokenizer)
+            print("Inputs are processed")
+            sentence_embeddings = generate_embeddings(inputs, self.model).detach().numpy().tolist()
+            print("Sentences are embedded")
             embeddings.append((sentence_embeddings, sentence, title))
+            
+            # Calculate and print the percentage completion
+            percent_complete = ((i + 1) / num_sentences) * 100
+            print(f"Processed {i + 1}/{num_sentences} sentences. {percent_complete:.2f}% complete")
 
         collection_name = 'BERT_embeddings'
         for embedding, sentence, title in embeddings:
@@ -46,10 +54,13 @@ class ChromaEmbeddingProcessor:
             except Exception as e:
                 print(f"Failed to insert document: {e}")
 
+
+
 # Example usage:
 if __name__ == "__main__":
     model_path = 'Checkpoints/checkpoint.pt'
+    tokenizer_path = 'tokenizer/wordPieceVocab.json'
     csv_file = 'dataset/dataset_sentences.csv'
 
-    processor = ChromaEmbeddingProcessor(model_path, csv_file)
+    processor = ChromaEmbeddingProcessor(model_path, tokenizer_path, csv_file)
     processor.process_and_store_embeddings()
