8b6cabc50dbf579a5df61abee583b0d71b00dd6c
Arnau Gris <arnaugris97@gmail.com>
2024-07-07 17:50:04 +0200

    Add llm, prompting and UI sections

--- ui_app.py
+++ ui_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from API_secrets import OPENAI_API_KEY
+from API_secrets import ANTROPHIC_API_KEY, OPENAI_API_KEY
 from createTL_embedding_DB import MilvusEmbeddingProcessorTL
 from inference_utils import load_model, preprocess_input, generate_embeddings, search_chromadb, generate_output
 from langchain_community.llms import Ollama
 from langchain_openai import OpenAI
+import anthropic
 
 model_path = 'Checkpoints/TL100Epochs.pt'
 tokenizer_path = 'tokenizer/wordPieceVocab.json'
@@ -13,6 +14,10 @@ llm = Ollama(model="llama3")
 
 llmGPT = OpenAI(api_key=OPENAI_API_KEY, model="gpt-3.5-turbo-instruct", temperature=0)
 
+client = anthropic.Anthropic(
+    api_key=ANTROPHIC_API_KEY,
+)
+
 st.title("Fairy Tale Generator")
 # Creating tabs
 tab1, tab2 = st.tabs(["Fairy Tale Generator", "Detailed View"])
@@ -20,7 +25,7 @@ tab1, tab2 = st.tabs(["Fairy Tale Generator", "Detailed View"])
 userPrompt = st.text_input("Enter a prompt to generate a fairy tale:", "Once upon a time there was a little red riding hood who lived in a small village.")
 
 # Adding a toggle button to select between Llama 3 and ChatGPT
-model_choice = st.radio("Select the inference model:", ("Llama 3", "ChatGPT"))
+model_choice = st.radio("Select the inference model:", ("Llama 3", "GPT 3.5 Turbo", "Claude 3.5"))
 
 if st.button("Generate Fairy Tale"):
     with st.spinner('Generating fairy tale...'):
@@ -41,8 +46,17 @@ if st.button("Generate Fairy Tale"):
             # Using the selected model for inference
             if model_choice == "Llama 3":
                 llm_result = llm.invoke(prompt_template)
-            else:
+            elif model_choice == "ChatGPT":
                 llm_result = llmGPT.invoke(prompt_template)
+            else :
+                message = client.messages.create(
+                    model="claude-3-5-sonnet-20240620",
+                    max_tokens=1024,
+                    messages=[
+                        {"role": "user", "content": prompt_template}
+                    ]
+                )
+                llm_result = message.content[0].text
             
 
              # Tab 1: Fairy Tale Generator
